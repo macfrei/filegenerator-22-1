440bd0e5f2fb531ff8ec7f46660e636f42ab7e9e
Marie-Carleen Frei <lene@7freiheiten.de>
2022-02-21 15:47:22 +0100

    Create new file with file system

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 console.log('Hello World!');
+const fs = require('fs');
+// import fs from 'fs'
 
 const functionName = process.argv[2];
 
 console.log(`function ${functionName}() {
-    
+
 }`);
+
+fs.writeFileSync('./math.js', '1+2=3');
