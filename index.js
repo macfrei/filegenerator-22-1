979da5c5737eb9aaa2b81db55835562b342400dd
Marie-Carleen Frei <lene@7freiheiten.de>
2022-02-21 16:16:15 +0100

    Add readline module to ask for function name

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const fs = require('fs');
-// import fs from 'fs'
-
-const functionName = process.argv[2];
 
 function writeFile(name) {
   fs.writeFileSync(
     `./${name}.js`,
     `function ${name}() {
-    
+
     }`
   );
 }
 
-writeFile(functionName);
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+
+const rl = readline.createInterface({ input, output });
+
+rl.question('What is the name of your function? ', answer => {
+  // Call writeFile function to create a new file with answer
+  writeFile(answer);
+  console.log(answer);
+
+  rl.close();
+});
