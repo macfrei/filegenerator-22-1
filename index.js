44040100a537d6db4bc6a1e1f149374ddbf61086
Marie-Carleen Frei <lene@7freiheiten.de>
2022-02-22 10:44:47 +0100

    Add inquirer to index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const writeFile = require('./writeFile');
+const inquirer = require('inquirer');
 
-const readline = require('readline');
-const { stdin: input, stdout: output } = require('process');
-
-const rl = readline.createInterface({ input, output });
-
-rl.question('What is the name of your function? ', answer => {
-  // Call writeFile function to create a new file with answer
-  writeFile(answer);
-  console.log(answer);
-
-  rl.close();
-});
-
-// Bonus: writeFile in own file and exportieren and import
+inquirer
+  .prompt([
+    {
+      name: 'functionName',
+      message: 'What the name of the function?',
+    },
+  ])
+  .then(answers => {
+    writeFile(answers.functionName);
+  });
