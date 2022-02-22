21580f18b951a4fcce0482eed4ddf82b6ea96158
Marie-Carleen Frei <lene@7freiheiten.de>
2022-02-22 11:13:04 +0100

    Add checkbox question with validation

--- index.js
+++ index.js
@@ -7,7 +7,20 @@ inquirer
       name: 'functionName',
       message: 'What the name of the function?',
     },
+    {
+      type: 'checkbox',
+      name: 'fileTypes',
+      message: 'Which file types should be created?',
+      choices: ['Component', 'Spec', 'Stories'],
+      validate: answer => {
+        if (answer.length < 1) {
+          return 'You must select at least one type!';
+        }
+        return true;
+      },
+    },
   ])
   .then(answers => {
+    console.log(answers);
     writeFile(answers.functionName);
   });
