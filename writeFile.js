36db2c40873f6618de5ccae9ba2f9ceee82e17fa
Marie-Carleen Frei <lene@7freiheiten.de>
2022-02-22 11:50:18 +0100

    Add template to writeFile function

--- writeFile.js
+++ writeFile.js
@@ -1,15 +1,24 @@
 const fs = require('fs');
 
+const template = {
+  component: name => {
+    const componentString = `import styled from 'styled-components'
+
+        export default function ${name}() {
+            return <div>${name}</div>
+        }
+        `;
+    return componentString;
+  },
+};
+
 function writeFile(name, fileType) {
   const fileName =
     fileType === 'component' ? `./${name}.js` : `./${name}.${fileType}.js`;
 
-  fs.writeFileSync(
-    fileName,
-    `function ${name}() {
-  
-  }`
-  );
+  const fileString = template[fileType](name); // template['component']
+
+  fs.writeFileSync(fileName, fileString);
 }
 
 module.exports = writeFile;
